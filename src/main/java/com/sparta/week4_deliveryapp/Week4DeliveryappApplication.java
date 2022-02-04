a7bd9b6cf5534af296cec98fc8eaa6f6005bf1b2
hyeonjh <97426920+hyeonjh@users.noreply.github.com>
2022-02-04 16:17:21 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week4_deliveryapp/Week4DeliveryappApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week4_deliveryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4DeliveryappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4DeliveryappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/week4_deliveryapp/Week4DeliveryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week4_deliveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4DeliveryappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
